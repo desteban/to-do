24d89e34715f647836f2f992b3e7310346333715
desteban <esteban28ec@outlook.com>
2024-08-21 21:51:48 -0500

    poner titulo cuando se completan todas las tareas

--- src/Components/Todo/TodoCounter.jsx
+++ src/Components/Todo/TodoCounter.jsx
@@ -11,11 +11,21 @@ export default function TodoCounter({ total, complete }) {
   if (total === 0) {
     return (
       <div className="text-center my-4">
-        <h2>Actualmente no tienes tarea</h2>
+        <h2>Actualmente no tienes tareas</h2>
         <p>Parece que no tienes tareas, puedes agregar una tarea nueva</p>
       </div>
     );
   }
+
+  if (total === complete) {
+    return(
+    <div className="text-center my-4">
+      <p>Parece que puedes irte a casa temprano </p>
+      <p>Has <b>terminado todas tus tareas</b></p>
+      </div>
+    )
+  }
+
   return (
     <div className="text-center my-4">
       <h2>
